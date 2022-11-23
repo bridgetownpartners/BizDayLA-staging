8716aac2c5a859697ee82d69d1b06ddf70766c9e
Edward.dev <jjeffasen@gmail.com>
2022-11-23 09:47:44 +0200

    fix: move automated images and make requests dynamic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var redirectUrl = "https://www.youtube.com/c/BizDayGlobal/videos";
-var imgDir = "./assets/automated-images/";
-var url = "https://api.github.com/repos/bridgetownpartners/BizDayGlobal-live/git/trees/f60091048b074180d948b5bc62a1f2ccefcfa879"; // master/assets/automated-images
+var imgDir = "./automated-images/";
+var url = "https://api.github.com/repos/bridgetownpartners/BizDayGlobal-live/git/trees/master"; // master/assets/automated-images
 var fileextension = ".png";
 var fileextensionJpg = ".jpg";
 var fileextensionJpeg = ".jpeg";
@@ -13,17 +13,30 @@ $.ajax({
     beforeSend: function (xhr) {
         xhr.setRequestHeader("Authorization", "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")
     },
-    success: function (data) {
-        data.tree.map((imageObj) => {
-            if (imageObj.path.includes(fileextension) || imageObj.path.includes(fileextensionJpg) || imageObj.path.includes(fileextensionJpeg)) {
-                $('#biz-day-card__wrapper').append(
-                    "<a href='" + redirectUrl + "' target='_blank' class='bizday_card_topic bizday_card_topic--has-image' style='background-image: url(" + imgDir + encodeURIComponent(imageObj.path) + "); background-size: cover; background-repeat: no-repeat; background-position: center center;'></a>"
-                );
+    success: function (automatedImages) {
+        automatedImages.tree.map((automatedImage) => {
+            if (automatedImage.path === 'automated-images') {
+                $.ajax({
+                    url: automatedImage.url,
+                    cors: true,
+                    type: 'GET',
+                    secure: true,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "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")
+                    },
+                    success: function (autoImages) {
+                        console.log(autoImages)
+                        autoImages.tree.map((imageObj) => {
+
+                            if (imageObj.path.includes(fileextension) || imageObj.path.includes(fileextensionJpg) || imageObj.path.includes(fileextensionJpeg)) {
+                                $('#biz-day-card__wrapper').append(
+                                    "<a href='" + redirectUrl + "' target='_blank' class='bizday_card_topic bizday_card_topic--has-image' style='background-image: url(" + imgDir + encodeURIComponent(imageObj.path) + "); background-size: cover; background-repeat: no-repeat; background-position: center center;'></a>"
+                                );
+                            }
+                        });
+                    }
+                })
             }
-        });
-    },
-    error: function (error, a) {
-        console.log(error);
-        console.log(error.responseText);
+        })
     }
 });
